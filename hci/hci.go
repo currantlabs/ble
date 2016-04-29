4942c7126d0c1ccacd85252b8c0633f59b6b9353
Tzu-Jung Lee <roylee17@currant.com>
2016-04-28 23:26:41 -0700

    hci: update default params for more responsive scanning/advertising

--- hci/hci.go
+++ hci/hci.go
@@ -98,37 +98,37 @@ func (h *HCI) Init(id int) error {
 	h.subh = map[int]handlerFn{}
 
 	h.scanParams = cmd.LESetScanParameters{
-		LEScanType:           0x01,   // [0x00]: passive, 0x01: active
-		LEScanInterval:       0x0010, // [0x10]: 0.625ms * 16
-		LEScanWindow:         0x0010, // [0x10]: 0.625ms * 16
-		OwnAddressType:       0x00,   // [0x00]: public, 0x01: random
-		ScanningFilterPolicy: 0x00,   // [0x00]: accept all, 0x01: ignore non-white-listed.
+		LEScanType:           0x01,   // 0x00: passive, 0x01: active
+		LEScanInterval:       0x0004, // 0x0004 - 0x4000; N * 0.625msec
+		LEScanWindow:         0x0004, // 0x0004 - 0x4000; N * 0.625msec
+		OwnAddressType:       0x00,   // 0x00: public, 0x01: random
+		ScanningFilterPolicy: 0x00,   // 0x00: accept all, 0x01: ignore non-white-listed.
 	}
 
 	h.advParams = cmd.LESetAdvertisingParameters{
-		AdvertisingIntervalMin:  0x010,     // [0x0800]: 0.625 ms * 0x0800 = 1280.0 ms
-		AdvertisingIntervalMax:  0x010,     // [0x0800]: 0.625 ms * 0x0800 = 1280.0 ms
-		AdvertisingType:         0x00,      // [0x00]: ADV_IND, 0x01: DIRECT(HIGH), 0x02: SCAN, 0x03: NONCONN, 0x04: DIRECT(LOW)
-		OwnAddressType:          0x00,      // [0x00]: public, 0x01: random
-		DirectAddressType:       0x00,      // [0x00]: public, 0x01: random
+		AdvertisingIntervalMin:  0x0020,    // 0x0020 - 0x4000; N * 0.625 msec
+		AdvertisingIntervalMax:  0x0020,    // 0x0020 - 0x4000; N * 0.625 msec
+		AdvertisingType:         0x00,      // 00: ADV_IND, 0x01: DIRECT(HIGH), 0x02: SCAN, 0x03: NONCONN, 0x04: DIRECT(LOW)
+		OwnAddressType:          0x00,      // 0x00: public, 0x01: random
+		DirectAddressType:       0x00,      // 0x00: public, 0x01: random
 		DirectAddress:           [6]byte{}, // Public or Random Address of the Device to be connected
-		AdvertisingChannelMap:   0x7,       // [0x07] 0x01: ch37, 0x2: ch38, 0x4: ch39
+		AdvertisingChannelMap:   0x7,       // 0x07 0x01: ch37, 0x2: ch38, 0x4: ch39
 		AdvertisingFilterPolicy: 0x00,
 	}
 
 	h.connParams = cmd.LECreateConnection{
-		LEScanInterval:        0x0004,    // N * 0.625 msec
-		LEScanWindow:          0x0004,    // N * 0.625 msec
+		LEScanInterval:        0x0004,    // 0x0004 - 0x4000; N * 0.625 msec
+		LEScanWindow:          0x0004,    // 0x0004 - 0x4000; N * 0.625 msec
 		InitiatorFilterPolicy: 0x00,      // White list is not used
 		PeerAddressType:       0x00,      // Public Device Address
 		PeerAddress:           [6]byte{}, //
 		OwnAddressType:        0x00,      // Public Device Address
-		ConnIntervalMin:       0x0006,    // N * 1.25 msec
-		ConnIntervalMax:       0x0006,    // N * 1.25 msec
-		ConnLatency:           0x0000,    // N * 1.25 msec
-		SupervisionTimeout:    0x0048,    // N * 10 msec
-		MinimumCELength:       0x0000,    // N * 0.625 msec
-		MaximumCELength:       0x0000,    // N * 0.625 msec
+		ConnIntervalMin:       0x0006,    // 0x0006 - 0x0C80; N * 1.25 msec
+		ConnIntervalMax:       0x0006,    // 0x0006 - 0x0C80; N * 1.25 msec
+		ConnLatency:           0x0000,    // 0x0000 - 0x01F3; N * 1.25 msec
+		SupervisionTimeout:    0x0048,    // 0x000A - 0x0C80; N * 10 msec
+		MinimumCELength:       0x0000,    // 0x0000 - 0xFFFF; N * 0.625 msec
+		MaximumCELength:       0x0000,    // 0x0000 - 0xFFFF; N * 0.625 msec
 	}
 
 	h.evth[0x3E] = h.handleLEMeta
