ae6b412471d202f3b680c704fa85480a6495edc3
Tzu-Jung Lee <roylee17@currant.com>
2016-06-29 15:00:47 -0700

    linux: add dialer and listener timeout options

--- linux/hci/option.go
+++ linux/hci/option.go
@@ -1,11 +1,37 @@
 package hci
 
 import (
+	"time"
+
 	"github.com/currantlabs/ble/linux/hci/cmd"
 )
 
 // An Option is a configuration function, which configures the device.
 type Option func(*HCI) error
+
+// OptDeviceID sets HCI device ID.
+func OptDeviceID(id int) Option {
+	return func(h *HCI) error {
+		h.id = id
+		return nil
+	}
+}
+
+// OptDialerTimeout sets dialing timeout for Dialer.
+func OptDialerTimeout(d time.Duration) Option {
+	return func(h *HCI) error {
+		h.dialerTmo = d
+		return nil
+	}
+}
+
+// OptListenerTimeout sets dialing timeout for Listener.
+func OptListenerTimeout(d time.Duration) Option {
+	return func(h *HCI) error {
+		h.listenerTmo = d
+		return nil
+	}
+}
 
 // OptScanParams sets scanning parameters.
 func OptScanParams(p ScanParams) Option {
@@ -23,7 +49,7 @@ func OptAdvParams(p AdvParams) Option {
 	}
 }
 
-// OptConnParams ...
+// OptConnParams sets connection parameters.
 func OptConnParams(p ConnParams) Option {
 	return func(h *HCI) error {
 		h.states.connParams = cmd.LECreateConnection(p)
